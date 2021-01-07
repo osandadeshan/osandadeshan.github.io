a7df7166ac2a573de2d7c342c062f329f47a03e6
Osanda Deshan <osanda.deshan@gmail.com>
2021-01-07 19:58:51 +0530

    Reverted the packaging

--- forms/contact.php
+++ forms/contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
