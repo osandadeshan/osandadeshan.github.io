7c9df3037874719507b19ca32ac76ee9a75e7df3
Osanda Deshan <osanda.deshan@gmail.com>
2021-10-24 20:51:52 +0530

    Updated typing cursor color to green

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,7 +157,7 @@
     }
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #34b7a7}";
     document.body.appendChild(css);
   };
 
